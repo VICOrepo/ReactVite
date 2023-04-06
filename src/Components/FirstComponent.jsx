9f965ce75594930607d06626753d42955d584fab
VICOrepo <viral.jain.2019@ecajmer.ac.in>
2023-04-06 09:54:46 +0530

    first commit

---
+++ src/Components/FirstComponent.jsx
@@ -0,0 +1,12 @@
+// Class Component
+import React, { Component } from 'react'
+
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div>FirstComponent</div>
+    )
+  }
+}
+
+export default FirstComponent
